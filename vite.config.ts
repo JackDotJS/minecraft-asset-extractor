fa0bc6a50cb59f3a0e9ffdcc332e0bb3f048e53a
Robert <contact.e3nk6@aleeas.com>
2023-04-03 23:31:47 +0200

    Add basic PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,27 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: `/minecraft-asset-extractor/`
+  base: `/minecraft-asset-extractor/`,
+  publicDir: 'assets',
+  plugins: [
+    VitePWA({
+      injectRegister: 'auto',
+      manifest: {
+        "name": "Minecraft Asset Extractor",
+        "short_name": "MC Extractor",
+        "start_url": ".",
+        "display": "standalone",
+        "background_color": "#f48b2f",
+        "description": "A web app that aims to simplify the process of extracting Minecraft's built-in game assets, including things like textures, sounds, models, JSON data, and more. This app works entirely through your browser.",
+        "icons": [
+          {
+            "src": "icon144x.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          }
+        ]
+      }
+    })
+  ]
 });
