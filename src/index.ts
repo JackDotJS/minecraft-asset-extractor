a3c3d492025b572736aba826e111621d33056afb
Robert <contact.e3nk6@aleeas.com>
2023-04-10 00:08:44 +0200

    version search

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const output = document.querySelector(`#output`);
 const fileselect = document.querySelector(`#mcfolder`);
 const filedrop = document.querySelector(`#filezone`);
 const startbutton = document.querySelector(`#extract`);
+const versionlist = document.querySelector(`#version-list`);
+const snapshots = document.querySelector(`#snapshotversions`);
+const historical = document.querySelector(`#historicalversions`);
 
 let ghbuild = "";
 let ghrev = "";
@@ -46,6 +49,24 @@ Promise.all([
     `UserAgent: ${navigator.userAgent}`
   ].join(`\n`));
 }).catch(console.warn);
+
+async function fetchVersions(snapshots: boolean, historical: boolean) {
+  versionlist.innerHTML = ``;
+  fetch(`https://piston-meta.mojang.com/mc/game/version_manifest_v2.json`).then(async (result) => {
+    const json = await result.json();
+    json.versions.forEach((version: Object) => {
+      if (snapshots === false && /[0-9]+w|pre|rc|3D/gi.test(version.id)) return;
+      if (historical === false && /rd|c0.|a1.|b1.|inf/gi.test(version.id)) return;
+
+      let option = document.createElement("li");
+      option.tabIndex = 0;
+      option.innerHTML = version.id;
+      versionlist?.append(option);
+	  });
+  })
+}
+
+fetchVersions(false, false);
 
 /**
    * force reset file selection and extract buttons on page load to prevent weirdness
@@ -124,4 +145,12 @@ startbutton?.addEventListener(`click`, () => {
     // TODO: it'd probably be better to disable the button altogether in this case
     alert(`No files selected!`);
   }
+});
+
+snapshots?.addEventListener(`change`, () => {
+  fetchVersions(snapshots?.checked, historical?.checked)
+});
+
+historical?.addEventListener(`change`, () => {
+  fetchVersions(snapshots?.checked, historical?.checked)
 });
